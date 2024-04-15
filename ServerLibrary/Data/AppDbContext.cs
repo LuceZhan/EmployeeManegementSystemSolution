e2a60f419915fac68f03cc9534e9f27008df24e2
“[lucezhan]” <[nathalisboa@gmail.com]>
2024-04-15 18:15:56 -0300

    add migrations,libraries and repositories

---
+++ ServerLibrary/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BaseLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerLibrary.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) :  DbContext(options) 
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<GeneralDepartment> GeneralDepartment { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Town> Town { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<SystemRole> SystemRoles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+
+    }
+    
+}
---
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -0,0 +1,66 @@
+﻿using ServerLibrary.Repositories.Contracts;
+using BaseLibrary.Responses;
+using BaseLibrary.DTOs;
+using Microsoft.Extensions.Options;
+using ServerLibrary.Helpers;
+using ServerLibrary.Data;
+using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using Constants = ServerLibrary.Helpers.Constants;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class UserAccountRepository(IOptions<JwtSection> config, AppDbContext appDbContext) : IUserAccount
+    {
+        public async Task<GeneralResponse> CreateAsync(Register user)
+        {
+            if (user is null) return new GeneralResponse(false, " Este modelo é vazio");
+            var CheckUser = await FindUserByEmail(user.Email!);
+            if (CheckUser != null) return new GeneralResponse(false, "Usuario já registrado.");
+
+            //Save user
+            var applicationUser = await AddToDatabase(new ApplicationUser()
+            {
+                FullName = user.FullName,
+                Email = user.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(user.Password)
+            });
+
+            //check, creat and assing role 
+            var checkAdminRole = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Helpers.Constants.Admin));
+            if (checkAdminRole is null)
+            {
+                var createAdminRole = await AddToDatabase(new SystemRole() { Name = Constants.Admin });
+                await AddToDatabase(new UserRole() { RoleId = createAdminRole.Id, UserId = applicationUser.Id });
+                return new GeneralResponse(true, "Conta criada!");
+            }
+
+            var checkUserRole = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.User));
+            SystemRole response = new();
+            if (checkUserRole is null) 
+            {
+                response = await AddToDatabase(new SystemRole() { Name = Constants.User });
+                await AddToDatabase(new UserRole() { RoleId = response.Id, UserId = applicationUser.Id });
+            }
+            else
+            {
+                await AddToDatabase(new UserRole() { RoleId = checkUserRole.Id, UserId = applicationUser.Id });
+            }
+            return new GeneralResponse(true, "Conta criada!");
+        }
+
+        public Task<LoginResponse> SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+        private async Task<ApplicationUser> FindUserByEmail(string email) =>
+            await appDbContext.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(email!.ToLower()));
+
+        private async Task<T> AddToDatabase<T>(T model)
+        {
+            var result = appDbContext.Add(model!);
+            await appDbContext.SaveChangesAsync();
+            return (T)result.Entity;
+        }
+    }
+}
